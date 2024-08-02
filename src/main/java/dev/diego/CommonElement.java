a0388a4a022e48b37b8750feb5c0ec875325e39a
diegofdez56 <169026006+diegofdez56@users.noreply.github.com>
2024-08-02 11:27:06 +0200

    Update CommonElement.java

--- src/main/java/dev/diego/CommonElement.java
+++ src/main/java/dev/diego/CommonElement.java
@@ -7,7 +7,7 @@ public class CommonElement {
     public ArrayList<Integer> findCommonElements(ArrayList<Integer> list1, ArrayList<Integer> list2) {
         HashSet<Integer> set1 = new HashSet<>(list1);
         HashSet<Integer> set2 = new HashSet<>(list2);
-        set1.retainAll(set2); // Retiene solo los elementos comunes entre set1 y set2
+        set1.retainAll(set2);
         return new ArrayList<>(set1);
     }
 }
